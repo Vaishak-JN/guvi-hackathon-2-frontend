f2efe93e5e56bd4b9d8d975d252149c2ff7445cd
Vaishak <vaishakjnair.98@gmail.com>
2023-03-26 15:39:48 +0530

    api

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-      headers : {
-        'Content-Type': 'application/json',
-        authorization : `Bearer ${localStorage.getItem('token')}`
-      }
+  baseURL: 'https://guvi-hackathon-2-vaishak.onrender.com/',
+  headers: {
+    'Content-Type': 'application/json',
+    authorization: `Bearer ${localStorage.getItem('token')}`
+  }
 });
